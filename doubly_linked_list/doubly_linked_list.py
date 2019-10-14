a1bc233ceaba967a3ecfa2be1a3515ecd4c9cb4b
Agustina Sosa <agustinasf85@gmail.com>
2019-10-14 18:55:28 -0400

    dill_queue.py passes all test but test_dequeue_respects_order = ERROR

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -53,12 +53,10 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
         new_node = ListNode(value, None, self.head)
-
         if not self.head and not self.tail:
             self.head = new_node
             self.tail = new_node
         else:
-            # new_node.next = self.head  -- not needed (optional)
             self.head.prev = new_node
             self.head = new_node
         self.length += 1
@@ -76,12 +74,11 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
         new_node = ListNode(value, self.tail, None)
-
-        if not self.tail and not self.tail:
-            self.tail = new_node
+        if not self.head and not self.tail:
+            self.head = new_node
             self.tail = new_node
         else:
-            self.tail.prev = new_node
+            self.tail.next = new_node
             self.tail = new_node
         self.length += 1
 
@@ -96,41 +93,46 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
-    # NOTE : Nodes can delete themselves, this handles the metadata
-    def delete(self, node): # we dont need to check for length of list because we're calling a node as a param
+    # Note:  Nodes can delete themselves, this handles the metadata
+    def delete(self, node):
         self.length -= 1
-
         if self.head is self.tail:
             self.head = None
             self.tail = None
         elif node is self.head:
-            self.head = self.head.next # same as node.next
+            self.head = node.next
             node.delete()
         elif node is self.tail:
-            self.tail = node.prev # same as self.tail.prev
+            self.tail = node.prev
             node.delete()
         else:
             node.delete()
-        
+            
     """Returns the highest value currently in the list"""
     def get_max(self):
         if not self.head:
-            return None
-
+            return None    
         max_val = self.head.value
         current = self.head
-
         while current:
             if current.value > max_val:
-                max_val = current
+                max_val = current.value
             current = current.next
-            return max_val
+        return max_val
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,14 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if len(self.storage) > 0:
+            return self.storage.pop()
 
     def len(self):
-        pass
+        return len(self.storage)
